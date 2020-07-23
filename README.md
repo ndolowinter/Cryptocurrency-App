b12b8aec3f0ef4a144aabfda9bb621271f228f6b
ndolowinter <winters.shadrack@gmail.com>
2020-07-24 02:32:12 +0300

    Creating the wallet.

---
+++ README.md
@@ -0,0 +1,31 @@
+# CYPRIOCOIN 2020
+
+## Description
+This is a block-chain application that will be used to make the Cyprio-coin.
+
+## Technologies used.
+
+* Postman App.
+* Git bash.
+* Atom IDE.
+
+## Development
+Run all tests:
+
+```
+$ npm install
+$ npm test
+$ npm run dev
+```
+or on Windows...
+```
+$ npm install
+$ npm run test-windows
+$ npm run dev
+```
+
+## Bugs.
+The chain string is not being identified by websockets.
+
+## Collaborations.
+Currently working on building the app. Any open source collaborations are welcome.
---
+++ app/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
+const p2pServer = new P2pServer();
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+
+	p2pServer.syncChains();
+	
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+	res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
+});
+
+
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
+
+p2pServer.listen();
