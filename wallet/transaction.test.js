bed4c7562dd0307536b0303821b671f23636061d
ndolowinter <winters.shadrack@gmail.com>
2020-08-11 12:53:57 +0300

    Testing reward miners.

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,6 +1,7 @@
 
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const {MINING_REWARD} = require('../config');
 
 describe('Transaction', () => {
   let transaction, wallet, recipient, amount;
@@ -34,5 +35,17 @@ describe('Transaction', () => {
     it('does not create the transaction', () => {
       expect(transaction).toEqual(undefined);
     });
+});
+
+describe('creating a reward transaction', () => {
+  beforeEach(() => {
+    transaction = Transaction.rewardTransaction(wallet, Wallet.blockchainWallet());
+  });
+
+  it(`rewards the miner's wallet`, () => {
+    expect(transaction.outputs.find ( output => output.address === wallet.publicKey).amount)
+    .toEqual(MINING_REWARD);
+
+  });
 });
 });
