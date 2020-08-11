6d3af532aec7228b5fc6ee1c7a470781f3c5f8dc
ndolowinter <winters.shadrack@gmail.com>
2020-08-11 11:37:36 +0300

    Run test: Transactions pool test has bugs.

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -4,12 +4,11 @@ const Transaction = require('./transaction');
 const Wallet = require('./index');
 
 describe('TransactionPool', () => {
-  let tp, wallet, transaction;
+  let tp, bc, wallet, transaction;
   beforeEach(() => {
     tp = new TransactionPool();
     wallet = new Wallet();
-    transaction = Transaction.newTransaction(wallet, 'r4nd-4dr355', 30);
-    tp.updateOrAddTransaction(transaction);
+      transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
   });
 
   it('adds a transaction to the pool', () => {
@@ -23,4 +22,29 @@ describe('TransactionPool', () => {
     expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id)))
       .not.toEqual(oldTransaction);
   });
+
+  describe('mixing valid and corrupt transactions', () => {
+    let validTransactions;
+    beforeEach(() => {
+      validTransactions = [...tp.transactions];
+      for (let i=0; i<6; i++) {
+        wallet = new Wallet();
+        transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
+        if (i%2==0) {
+          transaction.input.amount = 9999;
+        } else {
+          validTransactions.push(transaction);
+        }
+      }
+    });
+
+    it('shows a difference between valid and corrupt transactions', () => {
+      expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
+    });
+
+    it('grabs valid transactions', () => {
+      expect(tp.validTransactions()).toEqual(validTransactions);
+    });
+  });
+
 });
